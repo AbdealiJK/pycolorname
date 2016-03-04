0e970e682502106bffbb096f001fc640e65dd39a
AbdealiJK <abdealikothari@gmail.com>
2016-03-04 21:40:59 +0530

    pycolorname/pantone: Add source PantonePaint
    The pantonepaint.co.kr is used as a source here to find all
    colors in the system. Some colors are unicode, and so Utilities
    for unicode functions has been created for py2 and py3
    compatibility.

---
+++ tests/pantone/PantonePaintTest.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pycolorname.pantone.PantonePaint import PantonePaint
+from pycolorname.Utilities import u
+
+
+class PantonePaintTest(unittest.TestCase):
+    def setUp(self):
+        self.uut = PantonePaint()
+
+    def test_data(self):
+        self.assertEqual(len(self.uut), 2095)
+        
+        # We check a few random colors to be sure that things are fine.
+        self.assertEqual(self.uut['PMS 19-5918 TPX (Mountain View)'],
+                         [48, 65, 54])
+        self.assertEqual(self.uut['PMS 14-2808 TPX (Sweet Lilac)'],
+                         [231, 184, 211])
+        self.assertEqual(self.uut['PMS 15-4717 TPX (Aqua)'],
+                         [99, 162, 176])
+        self.assertEqual(self.uut['PMS 16-1720 TPX (Strawberry Ice)'],
+                         [227, 134, 143])
+        self.assertEqual(self.uut['PMS 19-4056 TPX (Olympian Blue)'],
+                         [46, 84, 147])
+
+        # Check names with unicode are working correctly
+        self.assertEqual(self.uut[u('PMS 16-5919 TPX (Cr\xe9me de Menthe)')],
+                         [114, 162, 139])
