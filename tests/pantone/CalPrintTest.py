49fe3e73cd9b2d9290c8afb387f84d1660813cf0
AbdealiJK <abdealikothari@gmail.com>
2016-03-04 18:40:18 +0530

    pycolorname/pantone: Add CalPrint
    CalPrint uses the cal-print.com Ink chart to generate color names
    using the ColorSystem class.

---
+++ tests/pantone/CalPrintTest.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pycolorname.pantone.CalPrint import CalPrint
+
+
+class CalPrintTest(unittest.TestCase):
+    def setUp(self):
+        self.uut = CalPrint()
+
+    def test_data(self):
+        self.assertEqual(len(self.uut), 992)
+        self.assertIn("White (White)", self.uut)
+
+        # We check a few random colors to be sure that things are fine.
+        self.assertEqual(self.uut['PMS 245 (Pantone Purple)'],
+                         [232, 127, 201])
+        self.assertEqual(self.uut['PMS 202 (Pantone Rubine Red 2X)'],
+                         [140, 38, 51])
+        self.assertEqual(self.uut['Pantone Cool Gray 5 (Pantone Cool Gray 5)'],
+                         [186, 183, 175])
+        self.assertEqual(self.uut['PMS 1345 (Pantone Warm Gray 2)'],
+                         [255, 214, 145])
+        self.assertEqual(self.uut['Pantone Purple (Pantone Purple)'],
+                         [191, 48, 181])
+
+
+        # Check that the nearest color to named colors are themselves.
+        # As, delta_e for named colors with themselves should be minimum.
+        for name, color in self.uut.items():
+            if not name.startswith("PMS"):
+                original_name, nearest_name = name.split(" (")
+                nearest_name = nearest_name.replace(")", "")
+                self.assertEqual(original_name, nearest_name)
