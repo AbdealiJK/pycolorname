c06c93d447b6eeb33061bf493e4e37196de908ad
AbdealiJK <abdealikothari@gmail.com>
2016-03-04 10:10:10 +0530

    Remove current code in favour of classes
    Currently the code uses functions and is a little messy due to
    repetitive code and a lack of structure overall. This commit is
    part of a series of commits to change everything into a class based
    system.

--- pycolorname/__init__.py
+++ pycolorname/__init__.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 30 22:57:26 2012
 
-@author: drtrigon
-"""
+# TODO:
+# List of color systems to add:
 
-import RAL
-import pantone
+# Pantone color standard
+# - cal-print:
+#   - URL: http://www.cal-print.com/InkColorChart.htm
+# - logodesignteam:
+#   - URL: http://www.logodesignteam.com/logo-design-pantone-color-chart.html
+#          http://www.logodesignteam.com/css/all.css
+# - ackerdesign:
+#   - URL: http://www.ackerdesign.com/acker-design-pantone-chart.html
+# - materialsworld:
+#   - URL: http://www.materials-world.com/pantone/pantone.htm
+# - pantonepaint:
+#   - URL: http://www.pantonepaint.co.kr/color/colorchipsearch.asp?cmp=TPX
 
-# NCS (http://de.wikipedia.org/wiki/Natural_Color_System) not implemented ...
+# RAL
+# - wikipedia
+#   - URL: http://de.wikipedia.org/wiki/RAL-Farbe
+
+# DIC Color System Guide
+
+# NCS (Natural Color system)
+# URL: http://de.wikipedia.org/wiki/Natural_Color_System)
