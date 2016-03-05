2d66247702ae1158fdc4989e84b3feddc0e362b8
AbdealiJK <abdealikothari@gmail.com>
2016-03-05 05:33:28 +0530

    pycolorname/pantone: Add source LogoDesignTeam
    The site logodesignteam.com is used as a source here to find all
    colors in the system.

---
+++ tests/pantone/LogoDesignTeamTest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pycolorname.pantone.LogoDesignTeam import LogoDesignTeam
+
+
+class LogoDesignTeamTest(unittest.TestCase):
+    def setUp(self):
+        self.uut = LogoDesignTeam()
+
+    def test_data(self):
+        self.assertEqual(len(self.uut), 992)
+        self.assertEqual(self.uut['White'], [255, 255, 255])
+
+        # We check a few random colors to be sure that things are fine.
+        self.assertEqual(self.uut['PMS 245'], [232, 127, 201])
+        self.assertEqual(self.uut['PMS 202'], [140, 38, 51])
+        self.assertEqual(self.uut['Pantone Cool Gray 5'], [186, 183, 175])
+        self.assertEqual(self.uut['PMS 1345'], [255, 214, 145])
+        self.assertEqual(self.uut['Pantone Purple'], [191, 48, 181])
