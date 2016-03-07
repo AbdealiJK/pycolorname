96e8d3dfe381787c11fb687d2d72c22e33adf80e
AbdealiJK <abdealikothari@gmail.com>
2016-03-07 11:40:51 +0530

    deprecation_test: Test pantonepaint
    Figure out all exceptions and add test for pantonepaint.

--- tests/deprecation_test.py
+++ tests/deprecation_test.py
@@ -37,15 +37,55 @@ class DeprecationTest(unittest.TestCase):
                    "dict1 has the following extra items: {3}\n"
                    "dict2 has the following extra items: {4}\n"
                    .format(len(keys1), len(keys2), len(keys1 & keys2),
-                           pprint.pformat({k:dict1[k] for k in keys1 - keys2}),
-                           pprint.pformat({k:dict2[k] for k in keys2 - keys1})))
+                           pprint.pformat({k: dict1[k]
+                                           for k in keys1 - keys2}),
+                           pprint.pformat({k: dict2[k]
+                                           for k in keys2 - keys1})))
             raise AssertionError(msg)
         self.assertEqual(dict1, dict2)
 
-    # def test_pantone_pantonepaint(self):
-    #     pkl_colors = self.get_pkl_colors("PMS_pantonepaint_raw.pkl")
-    #     new_colors = PantonePaint()
-    #     self.compare_dicts(pkl_colors, new_colors)
+    def test_pantone_pantonepaint(self):
+        pkl_colors = self.get_pkl_colors("PMS_pantonepaint_raw.pkl")
+        new_comparable_colors = {}
+
+        for name, color in PantonePaint().items():
+            # Two color names are not there in the earlier set
+            if name in ("PMS 13-1520 TPX (Rose Quartz)",
+                        "PMS 15-3919 TPX (Serenity)"):
+                continue
+
+            # Earlier there was an extra Pantone which has been removed from
+            # the site now. This is unnecessary as we already have a PMS at
+            # the beginning
+            name = name[:-1] + " Pantone" + name[-1:]
+
+            # Earlier "&amp;" was not being converted to "&". Now it is.
+            name = name.replace("&", "&amp;")
+
+            # Earlier unicode characters like "é" were taken as "?".
+            name = name.encode("ascii", errors="replace").decode("utf-8")
+
+            # There were typos earlier in the website.
+            # Use the typo version again for comparison.
+            mapping = {
+                "PMS 19-4118 TPX (Dark Denim Pantone)":  # Denim -> Derim
+                    "PMS 19-4118 TPX (Dark Derim Pantone)",
+                "PMS 16-0439 TPX (Spinach Green Pantone)":  # Spinach->spinach
+                    "PMS 16-0439 TPX (spinach Green Pantone)",
+                "PMS 16-1343 TPX (Autumn Sunset Pantone)":  # Sunset -> Sunst
+                    "PMS 16-1343 TPX (Autumn Sunst Pantone)",
+                "PMS 17-0000 TPX (Frost Gray Pantone)":  # Gray -> Gary
+                    "PMS 17-0000 TPX (Frost Gary Pantone)",
+                # Chalcoal->Charcoal
+                "PMS 18-0601 TPX (Charcoal Gray Pantone)":
+                    "PMS 18-0601 TPX (Chalcoal Gray Pantone)"
+            }
+            if name in mapping:
+                name = mapping[name]
+
+            new_comparable_colors[name] = color
+
+        self.compare_dicts(pkl_colors, new_comparable_colors)
 
     # def test_pantone_logodesignteam(self):
     #     pkl_colors = self.get_pkl_colors("PMS_logodesignteam_raw.pkl")
@@ -61,4 +101,3 @@ class DeprecationTest(unittest.TestCase):
     #     pkl_colors = self.get_pkl_colors("RAL_wikipedia.pkl")
     #     new_colors = Wikipedia()
     #     self.compare_dicts(pkl_colors, new_colors)
-
